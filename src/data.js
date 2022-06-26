654e353810e9dcbf259a82152ba300d105740da0
MedikondaAnjali <anjali@hubilo.com>
2022-06-26 23:06:50 +0530

    Added my project

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Dell laptop',
+      price: 120400,
+      image: 'https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9pNS53YWxtYXJ0aW1hZ2VzLmNvbS9hc3IvOThjMDc5OTYtMjM4NC00MGJkLTkyZmEtYTZhNTg1NzliZjVkLjllNDNmNDc5NTI1NWI2MTQzMjVlYzFkOGYyOWMyYWFlLmpwZWc.jpg',
+    },
+    {
+      id: '2',
+      name: 'one plus phone',
+      price: 21999,
+      image: 'https://images-eu.ssl-images-amazon.com/images/I/41mDAtPMDKL._SX300_SY300_QL70_FMwebp_.jpg',
+    },
+    {
+      id: '3',
+      name: 'Urbane women dress',
+      price: 517,
+      image: 'https://images.meesho.com/images/products/103713364/0ioi6_512.jpg',
+    },
+    {
+      id: '4',
+      name: 'JBL Ear phones',
+      price: 399,
+      image: 'https://m.media-amazon.com/images/I/51Uzw-xj-4L._SX425_.jpg',
+    },
+    {
+      id: '5',
+      name: 'Face Mask',
+      price: 299,
+      image: 'https://m.media-amazon.com/images/I/61+ilDgVVwS._UX522_.jpg',
+    },
+  ],
+};
+export default data;
